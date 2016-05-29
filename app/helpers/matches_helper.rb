7a0e7ad4eeb0cc6321cfcb8c63bf7ab8ff55c6f8
Marta Wojtowicz <marta.wojtowicz@polcode.net>
2016-05-29 18:01:15 +0200

    match validations added

--- app/helpers/matches_helper.rb
+++ app/helpers/matches_helper.rb
@@ -1,2 +1,5 @@
 module MatchesHelper
+  def played_value(match)
+    (match.played || Time.now).strftime("%d.%m.%Y, %H:%M")
+  end
 end
--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,14 @@
 class Match < ActiveRecord::Base
   belongs_to :first_team,  class_name: "Team"
   belongs_to :second_team, class_name: "Team"
+
+  validates_presence_of :first_team, :second_team, :played
+  validate :check_teams
+
+  private
+  def check_teams
+    if first_team == second_team
+      errors[:add] << "You can't choose the same teams"
+    end
+  end
 end
