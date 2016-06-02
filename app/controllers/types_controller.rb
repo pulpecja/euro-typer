1bcd090b7d839cc3005ec0d263c4b8d536df43ed
Marta Wojtowicz <marta.wojtowicz@polcode.net>
2016-06-03 00:08:47 +0200

    types connected with rounds

--- app/controllers/types_controller.rb
+++ app/controllers/types_controller.rb
@@ -1,34 +1,27 @@
 class TypesController < ApplicationController
   before_action :set_type, only: [:show, :edit, :update, :destroy]
   before_action :set_round, only: [:index, :prepare]
+  before_action :set_matches, only: [:index]
+  before_action :prepare, only: [:index]
 
   load_and_authorize_resource except: [:prepare]
   skip_authorization_check only: [:prepare]
   respond_to :json
 
-  # GET /types
-  # GET /types.json
   def index
     @types = Type.by_user(current_user)
-    @matches = Match.by_round(@round)
   end
 
-  # GET /types/1
-  # GET /types/1.json
   def show
   end
 
-  # GET /types/new
   def new
     @type = Type.new
   end
 
-  # GET /types/1/edit
   def edit
   end
 
-  # POST /types
-  # POST /types.json
   def create
     @type = Type.new(type_params)
 
@@ -43,8 +36,6 @@ class TypesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /types/1
-  # PATCH/PUT /types/1.json
   def update
     respond_to do |format|
       if @type.update(type_params)
@@ -57,8 +48,6 @@ class TypesController < ApplicationController
     end
   end
 
-  # DELETE /types/1
-  # DELETE /types/1.json
   def destroy
     @type.destroy
     respond_to do |format|
@@ -68,15 +57,12 @@ class TypesController < ApplicationController
   end
 
   def prepare
-    @matches = Match.by_round(@round)
     @matches.each do |match|
       Type.find_or_create_by(user: current_user, match: match)
     end
-    redirect_to round_types_path(@round.id)
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_type
       @type = Type.find(params[:id])
     end
@@ -85,7 +71,10 @@ class TypesController < ApplicationController
       @round = Round.find(params[:round_id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    def set_matches
+      @matches = Match.by_round(@round)
+    end
+
     def type_params
       params.require(:type).permit(:user_id, :match_id, :first_score, :second_score)
     end
