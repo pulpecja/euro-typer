4ccb203450a75a56b6d230feb1477cddd8d41767
Marta Wojtowicz <marta.wojtowicz@polcode.net>
2016-06-04 20:08:05 +0200

    added can be played method to types

--- app/helpers/types_helper.rb
+++ app/helpers/types_helper.rb
@@ -14,4 +14,12 @@ module TypesHelper
       }
     }
   end
+
+  def round_link(adjective, round_id)
+    link_to "#{adjective} kolejka", round_types_path(round_id) if Round.where(id: round_id).present?
+  end
+
+  def can_be_typed?(played)
+    played.in_time_zone > (DateTime.now.in_time_zone + 4.hours)
+  end
 end
