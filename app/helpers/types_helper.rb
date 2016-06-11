4df5f6b90f2ffd19c67081595e06a4b1538e6bfa
Marta Wojtowicz <marta.wojtowicz@polcode.net>
2016-06-11 11:42:03 +0200

    types hidden

--- app/helpers/types_helper.rb
+++ app/helpers/types_helper.rb
@@ -20,7 +20,7 @@ module TypesHelper
   end
 
   def can_be_typed?(played)
-    played.in_time_zone > (DateTime.now.in_time_zone + 4.hours)
+    played.in_time_zone > (DateTime.now.in_time_zone + 3.hours)
   end
 
   def good_bet(match, type)
