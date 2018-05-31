72bf91d447e2fad029a0e4170df9aacdd0581d00
pulpecja <marta.wojtowicz@polcode.net>
2018-05-31 18:57:54 +0200

    assets compiled

---
+++ spec/services/group_service_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+
+RSpec.describe GroupService do
+
+    before(:each) do
+    @user  = create :user
+    # @group = create :group
+    sign_in(@user, scope: :user)
+  end
+
+  subject { GroupService.new(params, group) }
+
+      let(:group) { create :group }
+      let(:competition) { create :competition }
+      let(:params) { { group: {
+                         competition_ids: [competition.id] } } }
+
+  describe "passing valid parameters" do
+
+    it "is successful" do
+      expect(subject.call).to be_success
+    end
+
+    # it "creates invoice" do
+    #   invoice = subject.call.invoice
+    #   expect(invoice).to be_persisted
+    #   expect(invoice.company_name).to eq("ACME Corp.")
+    # end
+
+    # it "notifies via notification center" do
+    #   expect(notification_center).to receive(:invoice_created)
+    #   subject.call
+    # end
+end
+
+
+#   it 'initialize without exception' do
+#     assert_nothing_raised do
+#       base = GroupService.new
+#     end
+#   end
+
+#   it '#add_competitions adds competitions' do
+#     assert_nothing_raised do
+
+#       base = GroupService.new(params, group)
+#       base.add_competitions
+# binding.pry
+#       # assert base.respond_to?(:initialize_driver)
+#       # assert base.respond_to?(:get_page)
+#       # assert base.respond_to?(:close_browser)
+#     end
+#   end
+end
