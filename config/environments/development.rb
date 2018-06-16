b49b2e7685da4790b98a28ad566b97bfd251c489
pulpecja <marta.wojtowicz@polcode.net>
2018-06-16 13:16:03 +0200

    password recovery added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: '127.0.0.1', port: 3000, from: 'pulpecja@euro-typer.com' }
+  config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
 
   # Print deprecation notices to the Rails logger.
