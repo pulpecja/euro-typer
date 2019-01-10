47a058e23a7711ff0b3d5625a0e2c1cee59f0f68
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2019-01-10 15:40:13 +0100

    Add new methods to request helper

--- spec/support/request_helper.rb
+++ spec/support/request_helper.rb
@@ -1,6 +1,46 @@
 module RequestHelper
-  def json
-    @json ||= JSON.parse(response.body)
+  def json_data
+    json['data']
+  end
+
+  def json_included
+    json['included']
+  end
+
+  def json_id
+    json_data['id']
+  end
+
+  def json_attributes_underscored
+    json_data['attributes'].keys.each_with_object({}) do |k, hash|
+      hash[k.to_s.underscore] = json_data['attributes'][k]
+    end
+  end
+
+  def json_attributes
+    json_data['attributes'].keys.each_with_object({}) do |k, hash|
+      hash[k.to_s] = json_data['attributes'][k]
+    end
+  end
+
+  def json_errors
+    json['errors']
+  end
+
+  def json_included_ids
+    json_included.map { |inc| inc['id'] }
+  end
+
+  def json_included_attributes
+    json_included.map do |inc|
+      inc['attributes'].keys.each_with_object({}) do |k, hash|
+        hash[k.to_s.underscore] = inc['attributes'][k]
+      end
+    end
+  end
+
+  def json_meta
+    json['meta']
   end
 
   def clear_json
@@ -15,5 +55,10 @@ module RequestHelper
     JSON.parse(
       ActiveModelSerializers::SerializableResource.new(object).to_json
     )
+  end
+
+  private
+  def json
+    @json ||= JSON.parse(response.body)
   end
 end
