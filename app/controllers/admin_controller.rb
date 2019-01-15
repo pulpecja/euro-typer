4352b5edf57914ea158468266de9877e9e6b95f1
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2019-01-15 12:32:04 +0100

    Update abilities for namespace

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,4 +4,9 @@ class AdminController < ApplicationController
   rescue_from CanCan::AccessDenied do |exception|
     render json: { message: exception.message }, status: 403
   end
+
+  def current_ability
+    controller_namespace = self.class.name.split("::").first
+    @current_ability ||= Ability.new(current_user, controller_namespace)
+  end
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,17 +1,22 @@
 class Ability
   include CanCan::Ability
 
-  def initialize(user)
-    user ||= User.new # guest user (not logged in)
-    if user.is_admin?
-      can :manage, :all
-    elsif user.is_registered?
-      can :manage, Type
-      can :read, [Competition, Match, Page, Team, User]
-      can [:read, :create], Group
-      can [:update, :destroy], Group, owner_id: user.id
-      can :create, WinnerType
-      can :update, WinnerType, user_id: user.id
-    end
+  def initialize(user, namespace = nil)
+    user ||= User.new
+    case namespace
+      when 'Admin'
+        can :manage, :all if user.is_admin?
+      else
+        if user.is_admin?
+          can :manage, :all
+        elsif user.is_registered?
+          can :manage, Type
+          can :read, [Competition, Match, Page, Team, User]
+          can [:read, :create], Group
+          can [:update, :destroy], Group, owner_id: user.id
+          can :create, WinnerType
+          can :update, WinnerType, user_id: user.id
+        end
+      end
   end
 end
