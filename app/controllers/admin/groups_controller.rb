1d28e01300eecc3de9913ba4f822c3c9b57999e5
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2019-01-16 14:00:35 +0100

    Update groups controller

--- app/controllers/admin/groups_controller.rb
+++ app/controllers/admin/groups_controller.rb
@@ -1,73 +1,45 @@
 class Admin::GroupsController < AdminController
-  before_action :set_group, only: [:show, :edit, :update, :destroy]
+  before_action :set_group, only: [:show, :update, :destroy]
   load_and_authorize_resource
 
   def index
     @groups = Group.all
+    json_response(GroupSerializer, @groups)
   end
 
   def show
-  end
-
-  def new
-    @group = Group.new
-    @users = User.existing
-  end
-
-  def edit
-    @users = User.existing
+    json_response(GroupSerializer, @group)
   end
 
   def create
-    @group = Group.new(group_params)
-    add_users
-    add_competitions
-    if @group.save
-      flash[:notice] = "Grupa stworzona"
-      redirect_to(admin_groups_path)
-    else
-      flash[:error]  = "Nie udało się utworzyć grupy"
-      render action: 'new'
-    end
+    @group = Group.create!(group_params)
+    json_response(GroupSerializer, @group)
   end
 
   def update
-    add_users
-    add_competitions
-    if @group.update(group_params)
-      flash[:notice] = "Grupa zapisana"
-      redirect_to(admin_groups_path)
-    else
-      flash[:error]  = "Nie udało się wyedytować grupy."
-      render action: 'edit'
-    end
+    @group.update!(group_params)
+    json_response(GroupSerializer, @group)
   end
 
   def destroy
     @group.destroy
-    flash[:notice] = 'Grupa usunięta!'
-    redirect_to admin_groups_path
+    head :no_content
   end
 
   private
-    def set_group
-      @group = Group.find(params[:id])
-    end
+  def set_group
+    @group = Group.find(params[:id])
+  end
 
-    def group_params
-      params.require(:group).permit(:name, :token, :owner_id, user_ids: [], competition_ids: [])
-    end
-
-    def add_users
-      @group.users = []
-      params[:group][:user_ids]&.reject(&:empty?)&.each do |user_id|
-        @group.users << User.find(user_id)
-      end
-      @group.users << User.find(params[:group][:owner_id]) if params[:group][:owner_id].present?
-    end
-
-    def add_competitions
-      group = GroupService.new(params[:group], @group)
-      group.add_competitions
-    end
+  def group_params
+    params.require(:data).permit(
+      attributes: [
+        :name,
+        :token,
+        :owner_id,
+        user_ids: [],
+        competition_ids: []
+      ]
+    )
+  end
 end
