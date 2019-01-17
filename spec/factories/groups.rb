13f0a2253a3a7f6436061a28c7fe32b1ad598413
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2019-01-17 11:36:59 +0100

    Update group factory

--- spec/factories/groups.rb
+++ spec/factories/groups.rb
@@ -2,9 +2,6 @@ FactoryBot.define do
   factory :group do
     name { Faker::LordOfTheRings.character }
     token { "MyString" }
-
-    before(:create) do |g|
-      g.owner_id = create(:user).id
-    end
+    association :owner, factory: :user
   end
 end
