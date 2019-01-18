d931b86923af702667fd457f630c602a7473a2c9
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2019-01-18 11:39:28 +0100

    Update winner type controller

--- app/controllers/winner_types_controller.rb
+++ app/controllers/winner_types_controller.rb
@@ -1,52 +1,49 @@
 class WinnerTypesController < ApplicationController
-  before_action :set_winner_type, only: [:edit, :update]
-  before_action :set_competition, only: [:new, :create, :edit, :update]
+  before_action :set_winner_type, only: [:show, :update, :destroy]
+  load_and_authorize_resource
 
-  load_and_authorize_resource except: [:prepare]
-  skip_authorization_check only: [:prepare]
-
-  def new
-    @winner_type = WinnerType.new
+  def index
+    @winner_types = WinnerType.all.by_competition(params[:filter][:competition_id])
+    json_response(WinnerTypeSerializer, @winner_types)
   end
 
-  def edit
+  def show
+    json_response(WinnerTypeSerializer, @winner_type)
   end
 
   def create
-    @winner_type = WinnerType.new(winner_type_params)
-    @winner_type.user = current_user
-
-    respond_to do |format|
-      if @winner_type.save
-        format.html { redirect_to @competition, notice: 'Typ stworzony.' }
-      else
-        format.html { render :new }
-      end
-    end
+    @winner_type = WinnerType.create!(winner_type_params)
+    json_response(WinnerTypeSerializer, @winner_type)
   end
 
   def update
-    @winner_type.update(winner_type_params)
-    respond_to do |format|
-      if @winner_type.save
-        format.html { redirect_to @competition, notice: 'Typ zmieniony.' }
-      else
-        format.html { render :new }
-      end
-    end
+    @winner_type.update!(winner_type_params)
+    json_response(WinnerTypeSerializer, @winner_type)
+  end
+
+  def destroy
+    @winner_type.destroy
+    head :no_content
   end
 
   private
-    def set_winner_type
-      @winner_type = WinnerType.find(params[:id])
-    end
+  def set_winner_type
+    @winner_type = WinnerType.find(params[:id])
+  end
 
-    def winner_type_params
-      params.require(:winner_type).permit(:team_id, :competition_id)
-    end
+  def winner_type_params
+    params.require(:data).permit(
+      attributes: [
+        :team_id,
+        :competition_id,
+        :user_id
+      ]
+    )
+  end
 
-    def set_competition
-      @competition = Competition.find(params[:competition] || params.dig(:winner_type, :competition_id))
-    end
-
+  # Temp fix, need to be removed bc there is rescue in ApplicationController,
+  # but seems not to be working
+  rescue_from CanCan::AccessDenied do |exception|
+    render json: { message: exception.message }, status: 403
+  end
 end
