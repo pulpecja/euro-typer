3f02b045eb1021a354e5f91c01dc1443b0c56338
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2019-01-21 15:37:27 +0100

    Remove photos attachement

--- db/migrate/20160604212456_add_photo_to_teams.rb
+++
@@ -1,5 +0,0 @@
-class AddPhotoToTeams < ActiveRecord::Migration[5.2]
-  def change
-    add_column :teams, :photo, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -19,17 +19,17 @@ ActiveRecord::Schema.define(version: 2019_01_14_145152) do
     t.string "name"
     t.integer "year"
     t.string "place"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.datetime "start_date"
     t.datetime "end_date"
-    t.integer "winner_id"
+    t.bigint "winner_id"
     t.index ["winner_id"], name: "index_competitions_on_winner_id"
   end
 
   create_table "competitions_groups", force: :cascade do |t|
-    t.integer "competition_id"
-    t.integer "group_id"
+    t.bigint "competition_id"
+    t.bigint "group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["competition_id"], name: "index_competitions_groups_on_competition_id"
@@ -37,8 +37,8 @@ ActiveRecord::Schema.define(version: 2019_01_14_145152) do
   end
 
   create_table "competitions_users", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "competition_id"
+    t.bigint "user_id"
+    t.bigint "competition_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["competition_id"], name: "index_competitions_users_on_competition_id"
@@ -55,39 +55,34 @@ ActiveRecord::Schema.define(version: 2019_01_14_145152) do
   end
 
   create_table "groups_users", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "group_id"
+    t.bigint "user_id"
+    t.bigint "group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["group_id"], name: "index_groups_users_on_group_id"
     t.index ["user_id"], name: "index_groups_users_on_user_id"
   end
 
-  create_table "jwt_blacklist", force: :cascade do |t|
-    t.string "jti", null: false
-    t.index ["jti"], name: "index_jwt_blacklist_on_jti"
-  end
-
   create_table "matches", force: :cascade do |t|
     t.integer "first_team_id", null: false
     t.integer "second_team_id", null: false
     t.datetime "played", null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "round_id"
     t.integer "first_score"
     t.integer "second_score"
-    t.string "bet", limit: 255
+    t.string "bet"
     t.index ["first_team_id"], name: "index_matches_on_first_team_id"
     t.index ["round_id"], name: "index_matches_on_round_id"
     t.index ["second_team_id"], name: "index_matches_on_second_team_id"
   end
 
   create_table "rounds", force: :cascade do |t|
-    t.string "name", limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.datetime "started_at", default: "2016-06-20 00:00:00", null: false
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.datetime "started_at", default: "2019-01-21 00:00:00", null: false
     t.integer "competition_id"
     t.integer "stage"
     t.index ["competition_id"], name: "index_rounds_on_competition_id"
@@ -100,10 +95,10 @@ ActiveRecord::Schema.define(version: 2019_01_14_145152) do
 
   create_table "teams", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string "abbreviation", limit: 255
-    t.string "flag", limit: 255
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "abbreviation"
+    t.string "flag"
     t.string "name_en"
     t.text "photo"
   end
@@ -113,28 +108,28 @@ ActiveRecord::Schema.define(version: 2019_01_14_145152) do
     t.integer "match_id"
     t.integer "first_score"
     t.integer "second_score"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string "bet", limit: 255
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "bet"
     t.index ["match_id"], name: "index_types_on_match_id"
     t.index ["user_id"], name: "index_types_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username", limit: 255, null: false
-    t.string "email", limit: 255, default: "", null: false
-    t.string "encrypted_password", limit: 255, default: "", null: false
-    t.string "reset_password_token", limit: 255
+    t.string "username", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip", limit: 255
-    t.string "last_sign_in_ip", limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string "role", limit: 255
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "role"
     t.datetime "deleted_at"
     t.boolean "take_part", default: true
     t.string "provider", default: "email", null: false
@@ -148,9 +143,9 @@ ActiveRecord::Schema.define(version: 2019_01_14_145152) do
   end
 
   create_table "winner_types", force: :cascade do |t|
-    t.integer "team_id"
-    t.integer "user_id"
-    t.integer "competition_id"
+    t.bigint "team_id"
+    t.bigint "user_id"
+    t.bigint "competition_id"
     t.index ["competition_id"], name: "index_winner_types_on_competition_id"
     t.index ["team_id"], name: "index_winner_types_on_team_id"
     t.index ["user_id"], name: "index_winner_types_on_user_id"
