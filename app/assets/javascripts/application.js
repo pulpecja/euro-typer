e2834b3f1934dc0798f3421e7717f6e073b1e467
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2019-01-21 15:11:10 +0100

    remove js linking

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,4 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require bootstrap
-//= require editable
-//= require select2
-//= require clipboard
 //= require_tree .
