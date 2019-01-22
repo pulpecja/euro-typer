48b8930dca74efab45e9bd776fa715e9d2009441
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2019-01-22 10:02:44 +0100

    Revert "Remove protect_from_forgery method"
    This reverts commit ee48a464d8d77bb5c621bdf91f7e2cc2d9bd3db1.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   include Response
   include ExceptionHandler
+  protect_from_forgery with: :null_session, only: Proc.new { |c| c.request.format.json? }
 
   before_action :authenticate_user!
 
