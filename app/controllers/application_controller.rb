a94c21a9289d1235d475f80f67a8e32c1a4865c4
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2019-01-24 13:11:24 +0100

    Add new permitted params for user + change authenticate_user conditional

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,21 +4,16 @@ class ApplicationController < ActionController::API
   include Response
   include ExceptionHandler
 
-  before_action :authenticate_user!, unless: :auth_action
+  before_action :authenticate_user!, unless: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   rescue_from CanCan::AccessDenied do |exception|
     render json: { message: exception.message }, status: 403
   end
 
-  # before_action :configure_permitted_parameters, if: :devise_controller?
-
   protected
-  def auth_action
-    params[:controller] == 'devise_token_auth/sessions'
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :role])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :role])
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:username])
-  # end
 end
